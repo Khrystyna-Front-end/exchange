7e1805b2b8faf3e8b95c00e0e375c3c99dd2069c
Khrystyna-Front-end <121770905+Khrystyna-Front-end@users.noreply.github.com>
2023-06-26 16:35:59 +0300

    Create exchange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import dollar from "../src/img/dollar.jpg";
+import aust from "./img/aust dollar.png";
+// import moneyTime from "./img/MoneyTime.png";
+import logo from "./img/logo.png";
+// import uah from "./img/uah.png";
 
-function App() {
+export default function App() {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [convertedAmount, setConvertedAmount] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+    );
+    const data = await response.json();
+    setConvertedAmount(data.rates[toCurrency] * amount);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div class="blob-effect">
+        <span></span>
+        <span></span>
+        <span></span>
+        <div className="div-container" type="submit">
+          <h1>Money Time</h1>
+        </div>
+      </div>
+      <img src={logo} id="logo" />
+
+      {/* <img src={moneyTime} id="nameBand" /> */}
+
+      <img src={dollar} id="dollar" />
+      <img src={aust} id="aust" />
+      <img src={dollar} id="dollar" />
+      <div id="uah"></div>
+      <form onSubmit={handleSubmit}>
+        <label>Amount:</label>
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
+        />
+        <br />
+        <label>From:</label>
+        <select
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="JPY">JPY</option>
+        </select>
+        <br />
+        <label>To:</label>
+        <select
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+        >
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="GBP">GBP</option>
+          <option value="JPY">JPY</option>
+        </select>
+        <br />
+
+        <button type="submit">Convert</button>
+      </form>
+      {convertedAmount && (
+        <div>
+          <p>
+            Converted amount: {convertedAmount} {toCurrency}
+          </p>
+        </div>
+      )}
+
+      {/* <img src={uah} id="uah" /> */}
     </div>
   );
 }
-
-export default App;
